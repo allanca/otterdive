8e89330c9543364fe43a465b3c94f9e9caebb75b
Allan Carroll <allanca@gmail.com>
2011-02-09 12:09:04 -0800

    Serve handler now spits back face detection info instead of image.

--- otterdive/src/net/allancarroll/otterdive/OtterdiveServlet.scala
+++ otterdive/src/net/allancarroll/otterdive/OtterdiveServlet.scala
@@ -4,20 +4,12 @@ import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-import com.face.api.client.DefaultFaceClient;
 
 
 class OtterdiveServlet extends HttpServlet {
-	val API_KEY = "c967915c590dfba336487ece1150051c";
-	val API_SEC = "03c42ee0ea73d1485442f4e402191570";
-	
 	override def doGet(req: HttpServletRequest, resp: HttpServletResponse) = {
 		resp setContentType("text/plain")
 		resp.getWriter().println("Hello, world!")
-
-		val api = new DefaultFaceClient(API_KEY, API_SEC)
-		val photos = api.detect("http://farm3.static.flickr.com/2566/3896283279_0209be7a67.jpg")
-		print(photos)
 	}
 
 }
--- otterdive/src/net/allancarroll/otterdive/Serve.scala
+++ otterdive/src/net/allancarroll/otterdive/Serve.scala
@@ -4,16 +4,31 @@ import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+import com.face.api.client.DefaultFaceClient;
+
 import com.google.appengine.api.blobstore.BlobKey
 import com.google.appengine.api.blobstore.BlobstoreService
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+
 
 
 class Serve extends HttpServlet {
+	val API_KEY = "c967915c590dfba336487ece1150051c";
+	val API_SEC = "03c42ee0ea73d1485442f4e402191570";
     val blobstoreService = BlobstoreServiceFactory.getBlobstoreService()
 
 	override def doGet(req: HttpServletRequest, res: HttpServletResponse ) = {
         val blobKey = new BlobKey(req.getParameter("blob-key"))
-        blobstoreService.serve(blobKey, res)
+
+        val imagesService = ImagesServiceFactory.getImagesService()
+		val servingUrl = imagesService.getServingUrl(blobKey)
+		
+		print(servingUrl)
+        
+		val api = new DefaultFaceClient(API_KEY, API_SEC)
+		val photos = api.detect(servingUrl)
+		res.getWriter().print(photos)
     }
 }
